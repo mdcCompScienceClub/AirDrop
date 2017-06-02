d57bea57f1503b265d2321f755dc19ccd9adb606
Jessica Fuentes <jessicafuentes@Jessicas-MacBook-Air.local>
2017-06-02 12:31:05 -0400

    pushing lol

--- iBeacons/ViewController.swift
+++ iBeacons/ViewController.swift
@@ -18,7 +18,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     let motionManager = CMMotionManager()
     var bluetoothManager: CBPeripheralManager?
     var running: Bool = false
+    var beaconOneRSSI: String!
     var beaconTwoRSSI: String!
+    var beaconThreeRSSI: String!
+    var beaconFourRSSI: String!
+    var beaconFiveRSSI: String!
+    var beaconSixRSSI: String!
     let region = CLBeaconRegion(proximityUUID: UUID(uuidString: "B9407F30-F5F8-466E-AFF9-25556B575555")!, identifier: "Estimote")
     
     
@@ -50,6 +55,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
         
     }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -76,33 +84,62 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Dispose of any resources that can be recreated.
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return beaconStuff.count
     }
     
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRect(x: 20, y: 20, width: 10, height: 10))
+        headerView.backgroundColor = UIColor.lightGray
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50
+        return 75
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .value1, reuseIdentifier: "cell")
-        let type = beaconStuff[indexPath.row]
+        let type = beaconStuff[indexPath.section]
         switch type {
             case "Beacon 1 RSSI":
                 cell.textLabel?.text = type
+                if running {
+                    cell.detailTextLabel?.text = beaconOneRSSI
+            }
             case "Beacon 2 RSSI":
                 cell.textLabel?.text = type
                 if running {
                     cell.detailTextLabel?.text = beaconTwoRSSI
-                }
+            }
             case "Beacon 3 RSSI":
                 cell.textLabel?.text = type
+                if running {
+                    cell.detailTextLabel?.text = beaconThreeRSSI
+            }
             case "Beacon 4 RSSI":
                 cell.textLabel?.text = type
+                if running {
+                    cell.detailTextLabel?.text = beaconFourRSSI
+            }
             case "Beacon 5 RSSI":
                 cell.textLabel?.text = type
+                if running {
+                    cell.detailTextLabel?.text = beaconFiveRSSI
+            }
             case "Beacon 6 RSSI":
                 cell.textLabel?.text = type
+                if running {
+                    cell.detailTextLabel?.text = beaconSixRSSI
+            }
             case "Acceleration X":
                 cell.textLabel?.text = type
                 if running {
@@ -154,10 +191,31 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
         let knownBeacons = beacons.filter{$0.proximity != CLProximity.unknown }
         if knownBeacons.count > 0 {
+            print(beacons)
             for beacon in knownBeacons {
                 if (beacon.minor == 2) && (beacon.major == 2) {
+                    // Beacon IV
+                    beaconFourRSSI = "\(beacon.rssi)"
+                }
+                else if (beacon.minor == 1) && (beacon.major == 3) {
+                    // Beacon V
+                    beaconFiveRSSI = "\(beacon.rssi)"
+                }
+                else if (beacon.minor == 2) && (beacon.major == 3) {
+                    // Beacon VI
+                    beaconSixRSSI = "\(beacon.rssi)"
+                }
+                else if (beacon.minor == 1) && (beacon.major == 1) {
+                    // Beacon I
+                    beaconOneRSSI = "\(beacon.rssi)"
+                }
+                else if (beacon.minor == 2) && (beacon.major == 1) {
                     // Beacon II
                     beaconTwoRSSI = "\(beacon.rssi)"
+                }
+                else if (beacon.minor == 1) && (beacon.major == 2) {
+                    // Beacon III
+                    beaconThreeRSSI = "\(beacon.rssi)"
                 }
             }
         }
